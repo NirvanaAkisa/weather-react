5a7288e054dbdd5a06d7537429228271e58603ce
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-11-03 15:27:48 +0200

    Imported my project from codesandbox to vs code

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./App.css";
+import WeatherSearch from "./WeatherSearch";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className="App">
+      <h1>WeatherSearch</h1>
+    </div>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
